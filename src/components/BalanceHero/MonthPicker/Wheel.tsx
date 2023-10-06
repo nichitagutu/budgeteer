a396d33a524e0bea1533db4df9a31dc477451663
qpwedev <russfdk@gmail.com>
2023-10-06 17:06:01 +0200

    feat: month ios picker component

--- src/components/BalanceHero/MonthPicker/Wheel.tsx
+++ src/components/BalanceHero/MonthPicker/Wheel.tsx
@@ -4,6 +4,78 @@ import {
   TrackDetails,
   useKeenSlider,
 } from "keen-slider/react";
+import styled from "styled-components";
+
+const WheelContainer = styled.div`
+  display: block;
+  color: var(--tg-theme-text-color);
+  height: 100%;
+  overflow: visible;
+  width: 100%;
+
+  &.wheel--perspective-right .wheel__inner {
+    perspective-origin: calc(50% + 100px) 50%;
+    transform: translateX(10px);
+  }
+
+  &.wheel--perspective-left .wheel__inner {
+    perspective-origin: calc(50% - 100px) 50%;
+    transform: translateX(-10px);
+  }
+`;
+
+const WheelInner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  perspective: 1000px;
+  transform-style: preserve-3d;
+  height: 16%;
+  width: 100%;
+`;
+
+const WheelSlides = styled.div<{ width?: string }>`
+  height: 100%;
+  position: relative;
+  width: ${(props) => props.width || "100%"};
+`;
+
+const WheelShadow = styled.div<{ type: "top" | "bottom" }>`
+  background: ${(props) =>
+    props.type === "top"
+      ? "linear-gradient(to bottom, rgba(239, 239, 243, 0.9) 0%, rgba(239, 239, 243, 0.5) 100%)"
+      : "linear-gradient(to bottom, rgba(239, 239, 243, 0.5) 0%, rgba(239, 239, 243, 0.9) 100%)"};
+  left: 0;
+  height: calc(42% + 2px);
+  width: 100%;
+  border-bottom: ${(props) =>
+    props.type === "top" ? "0.5px solid rgba(255, 255, 255, 0.3)" : "none"};
+  border-top: ${(props) =>
+    props.type === "bottom" ? "0.5px solid rgba(255, 255, 255, 0.3)" : "none"};
+  position: relative;
+  margin-top: ${(props) => (props.type === "bottom" ? "2px" : "-2px")};
+  z-index: 5;
+`;
+
+const WheelLabel = styled.div`
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 1;
+  margin-top: 1px;
+  margin-left: 5px;
+`;
+
+const WheelSlide = styled.div`
+  align-items: center;
+  backface-visibility: hidden;
+  display: flex;
+  font-size: 20px;
+  font-weight: 400;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  justify-content: flex-end;
+`;
 
 export default function Wheel(props: {
   initIdx?: number;
@@ -13,6 +85,7 @@ export default function Wheel(props: {
   perspective?: "left" | "right" | "center";
   setValue?: (relative: number, absolute: number) => string;
   width: number;
+  onValueChange?: (value: string) => void;
 }) {
   const perspective = props.perspective || "center";
   const wheelSize = 20;
@@ -49,6 +122,18 @@ export default function Wheel(props: {
     },
     detailsChanged: (s) => {
       setSliderState(s.track.details);
+
+      if (props.onValueChange) {
+        const currentSlide = s.track.details.slides.find(
+          (slide) => slide.distance === 0
+        );
+
+        if (currentSlide) {
+          const i = currentSlide.abs;
+          const value = props.setValue ? props.setValue(i, i) : i;
+          props.onValueChange(value.toString());
+        }
+      }
     },
     rubberband: !props.loop,
     mode: "free-snap",
@@ -89,44 +174,32 @@ export default function Wheel(props: {
   }
 
   return (
-    <div
-      className={"wheel keen-slider wheel--perspective-" + perspective}
+    <WheelContainer
       ref={sliderRef}
+      className={`wheel wheel--perspective-${perspective}`}
     >
-      <div
-        className="wheel__shadow-top"
-        style={{
-          transform: `translateZ(${radius}px)`,
-          WebkitTransform: `translateZ(${radius}px)`,
-        }}
+      <WheelShadow
+        type="top"
+        style={{ transform: `translateZ(${radius}px)` }}
       />
-      <div className="wheel__inner">
-        <div className="wheel__slides" style={{ width: props.width + "px" }}>
+      <WheelInner>
+        <WheelSlides style={{ width: props.width + "px" }}>
           {slideValues().map(({ style, value }, idx) => (
-            <div className="wheel__slide" style={style} key={idx}>
+            <WheelSlide style={style} key={idx}>
               <span>{value}</span>
-            </div>
+            </WheelSlide>
           ))}
-        </div>
+        </WheelSlides>
         {props.label && (
-          <div
-            className="wheel__label"
-            style={{
-              transform: `translateZ(${radius}px)`,
-              WebkitTransform: `translateZ(${radius}px)`,
-            }}
-          >
+          <WheelLabel style={{ transform: `translateZ(${radius}px)` }}>
             {props.label}
-          </div>
+          </WheelLabel>
         )}
-      </div>
-      <div
-        className="wheel__shadow-bottom"
-        style={{
-          transform: `translateZ(${radius}px)`,
-          WebkitTransform: `translateZ(${radius}px)`,
-        }}
+      </WheelInner>
+      <WheelShadow
+        type="bottom"
+        style={{ transform: `translateZ(${radius}px)` }}
       />
-    </div>
+    </WheelContainer>
   );
 }
--- src/components/BalanceHero/MonthPicker/index.tsx
+++ src/components/BalanceHero/MonthPicker/index.tsx
@@ -1,42 +1,198 @@
-import { format, subDays } from "date-fns";
-import { useTheme } from "styled-components";
+import styled from "styled-components";
+import MonthPick from "../../../assets/month-pick-icon.svg";
+import { useState, useEffect } from "react";
 
 import Wheel from "./Wheel";
 
 import "./styles.css";
 
-export default function MonthPicker() {
-  function formateDate(_relative: number, absolute: number) {
-    return format(subDays(new Date(), absolute), "iii d LLL");
+const Overlay = styled.div<{ show: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  display: ${(props) => (props.show ? "block" : "none")};
+  z-index: 777;
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const Body = styled.div`
+  z-index: 2;
+  background-color: rgba(116, 116, 128, 0.12);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 100px;
+  padding: 5px 10px;
+  font-size: 12px;
+  max-width: 200px;
+  gap: 2px;
+`;
+
+const MonthPickIcon = styled.img`
+  margin-top: 2px;
+`;
+
+const Dropdown = styled.div<{ isOpen: boolean }>`
+  position: absolute;
+  top: 2rem;
+  left: -4rem;
+
+  display: flex;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  max-width: 600px;
+  z-index: 888;
+  opacity: 0;
+  transform: translateY(-10px);
+  visibility: hidden;
+  transition: opacity 0.25s ease, transform 0.25s ease, visibility 0.25s;
+
+  ${(props) =>
+    props.isOpen &&
+    `
+    opacity: 1;
+    transform: translateY(0);
+    visibility: visible;
+  `}
+
+  &::-webkit-scrollbar {
+    display: none;
   }
 
-  const theme = useTheme();
+  overflow: hidden;
+`;
+
+const WheelBody = styled.div`
+  height: 180px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${(props) => props.theme.secondary_bg_color};
+`;
+
+const WheelContainer = styled.div<{ width?: string; perspective?: string }>`
+  height: 180px;
+  width: ${(props) => props.width || "120px"};
+  perspective: ${(props) =>
+    props.perspective === "right"
+      ? "140px"
+      : props.perspective === "left"
+      ? "70px"
+      : "none"};
+`;
+
+export default function MonthPicker() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedMonth, setSelectedMonth] = useState(
+    new Date().toLocaleString("en-US", { month: "long" })
+  );
+  const [selectedYear, setSelectedYear] = useState(
+    new Date().getFullYear().toString()
+  );
+  const [tempSelectedMonth, setTempSelectedMonth] = useState(selectedMonth);
+  const [tempSelectedYear, setTempSelectedYear] = useState(selectedYear);
+
+  useEffect(() => {
+    setTempSelectedMonth(selectedMonth);
+    setTempSelectedYear(selectedYear);
+  }, [isDropdownOpen]);
+
+  const handleMonthChange = (value: string) => {
+    setTempSelectedMonth(value);
+  };
+  const handleYearChange = (value: string) => {
+    setTempSelectedYear(value);
+  };
 
   return (
-    <div
-      style={{
-        height: "240px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: theme.secondary_bg_color,
-      }}
-    >
-      <div style={{ width: 180, height: 180 }}>
+    <Container>
+      <Overlay
+        show={isDropdownOpen}
+        onClick={() => {
+          setDropdownOpen(false);
+          setSelectedMonth(tempSelectedMonth);
+          setSelectedYear(tempSelectedYear);
+        }}
+      />
+      <Body onClick={() => setDropdownOpen(!isDropdownOpen)}>
+        {selectedMonth} {selectedYear}
+        <MonthPickIcon src={MonthPick} alt="Month Picker" />
+      </Body>
+      <Dropdown isOpen={isDropdownOpen}>
+        <DatetimePicker
+          onMonthChange={handleMonthChange}
+          onYearChange={handleYearChange}
+          currentMonth={new Date().getMonth()}
+          currentYear={new Date().getFullYear()}
+        />
+      </Dropdown>
+    </Container>
+  );
+}
+
+function DatetimePicker({
+  onMonthChange,
+  onYearChange,
+  currentMonth,
+  currentYear,
+}: {
+  onMonthChange?: (value: string) => void;
+  onYearChange?: (value: string) => void;
+
+  currentMonth: number;
+  currentYear: number;
+}) {
+  function formatMonth(monthIndex: number) {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return monthNames[monthIndex];
+  }
+
+  function formatYear(_absolute: number, relativeYear: number): string {
+    return (currentYear - 100 + relativeYear).toString();
+  }
+
+  return (
+    <WheelBody>
+      <WheelContainer width="120px">
         <Wheel
-          loop
-          length={24}
+          length={12}
           width={140}
           perspective="right"
-          setValue={formateDate}
+          setValue={formatMonth}
+          initIdx={currentMonth}
+          onValueChange={onMonthChange}
         />
-      </div>
-      <div style={{ width: 70, height: 180 }}>
-        <Wheel loop length={24} width={23} />
-      </div>
-      <div style={{ width: 70, height: 180 }}>
-        <Wheel loop length={60} width={23} perspective="left" />
-      </div>
-    </div>
+      </WheelContainer>
+      <WheelContainer width="120px">
+        <Wheel
+          length={200}
+          width={70}
+          perspective="left"
+          setValue={formatYear}
+          initIdx={100}
+          onValueChange={onYearChange}
+        />
+      </WheelContainer>
+    </WheelBody>
   );
 }
