7d4c95418f77a0a59903902f1084203504d504e4
qpwedev <russfdk@gmail.com>
2023-10-09 13:04:41 +0200

    fix: user data validation

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 import { addUserQuery } from '../database/queries';
+import { ParsedInitData, withInitDataValidation } from '../validators/miniAppData';
 
-export const addUser = async (req: Request, res: Response) => {
+export const addUser = withInitDataValidation(async (req: Request, res: Response, initData: ParsedInitData) => {
     try {
-        const user = req.body;
-        const result = await addUserQuery(user);
+        const result = await addUserQuery(initData.user);
         res.status(201).json(result);
     } catch (error: any) {
         res.status(500).json({ error: error.message });
     }
-};
+});
